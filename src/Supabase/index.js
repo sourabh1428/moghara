c8159bbead2bc784f1028206799ddaa8aada95f3
Sourabh <sppathak1428@gmail.com>
2025-01-04 03:14:10 +0530

    deploy

--- src/Supabase/index.js
+++ src/Supabase/index.js
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Replace with your actual Supabase URL and API key
-const supabaseUrl = 'https://npycprqypwtohsmqamdt.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5weWNwcnF5cHd0b2hzbXFhbWR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU0NTk4MDcsImV4cCI6MjA1MTAzNTgwN30.CAHJNllbzzQlccyCcoypLQOihmGUq6hU6n1Htns_CMk';
+const supabaseUrl = import.meta.env.VITE_supabaseUrl;
+const supabaseKey = import.meta.env.VITE_supabaseKey
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
